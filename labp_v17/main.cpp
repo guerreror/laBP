5b586b32714214077426b83d83478b88bcf39dcc
Rafael Guerrero <rafguerr@129-79-223-228.dhcp-bl.indiana.edu>
2017-01-25 16:47:20 -0500

    Initial Commit

---
+++ labp_v17/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  labp_v17
+//
+//  Created by Rafael Guerrero on 1/25/17.
+//  Copyright © 2017 Rafael Guerrero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
